3a78eaf9e0e71e405c1e0d57311a7a27b7ac7703
santoshkumaawat <santoshkumaawat@gmail.com>
2021-03-22 17:09:14 +0530

    8

--- jQuery/jQuery.js
+++ jQuery/jQuery.js
@@ -1,45 +1,58 @@
 let cssStyles = {
     color: "yellow",
     background: "gray",
-    border: "3px solid red" 
+    border: "3px solid red"
 }
 let cssStyles2 = {
-    color: "blue",
-    background: "black",
-    border: "3px solid yellow" 
+    color: "red",
+    background: "yellow",
+    border: "3px solid green",
+    height: "25px"
 }
 let cssStyles3 = {
     color: "cyan",
     background: "gray",
-    border: "3px solid blue" 
+    border: "3px solid blue"
 }
 
 $("h1").css(cssStyles);
-$("p").css(cssStyles2);
+$("#para1").css(cssStyles2);
 $("ul").css(cssStyles3);
 
 //attr method
-$("img").attr("style","border: 3px solid red; border-radius:15px;");
+$("img").attr("style", "border: 3px solid red; border-radius:15px;");
 
 //val method
-$(".button1").click(function(){
-    alert("Hello "+$("#text").val());
+$(".button1").click(function() {
+    alert("Hello " + $("#text").val());
 });
 
 //addClass method
-$(".button2").click(function(){
+$(".button2").click(function() {
     $("h2").addClass("cssStyles4");
 });
 
 //removeClass method
-$(".button3").click(function(){
+$(".button3").click(function() {
     $("h2").removeClass("cssStyles4");
 });
 
-//toggleClass method // this class add and remove class both in one
-$(".button4").click(function(){
+//toggleClass method // this method add and remove class both in one
+$(".button4").click(function() {
     $("h2").toggleClass("cssStyles4");
 });
 
-
-
+//jQuery effects
+//hide
+$("#hide").click(function() {
+    $("#para2").hide();
+});
+//show
+$("#show").click(function() {
+    $("#para2").show();
+});
+//toggle
+$("#toggle").click(function() {
+    $("#para2").toggle();
+});
+//fadeOut
